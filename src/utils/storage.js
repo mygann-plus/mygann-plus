d60fdaebbbc99bcb9fb0eadec4ce707bc981bcee
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-02-02 08:24:22 -0500

    Export single storage object

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,20 +1,24 @@
 // Promise-based wrappers for chrome storage API
 
-export const get = function get(property, doSync = true) {
-  return new Promise(resolve => {
-    const handleGet = value => resolve(value[property]);
-    if (doSync) {
-      chrome.storage.sync.get(property, handleGet);
-    } else {
-      chrome.storage.local.set(property, handleGet);
-    }
-  });
-};
+export default {
 
-export const set = function set(property, doSync = true) {
-  if (doSync) {
-    chrome.storage.sync.set(property);
-  } else {
-    chrome.storage.local.set(property);
-  }
+  get: function get(property, doSync = true) {
+    return new Promise(resolve => {
+      const handleGet = value => resolve(value[property]);
+      if (doSync) {
+        chrome.storage.sync.get(property, handleGet);
+      } else {
+        chrome.storage.local.set(property, handleGet);
+      }
+    });
+  },
+
+  set: function set(property, doSync = true) {
+    if (doSync) {
+      chrome.storage.sync.set(property);
+    } else {
+      chrome.storage.local.set(property);
+    }
+  },
+
 };
