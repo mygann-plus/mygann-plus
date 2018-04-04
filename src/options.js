abcae7e84215e87833b0eb0f39c8c98df474e419
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-04-04 18:24:26 -0400

    Implement module loading in options

--- src/options.js
+++ src/options.js
@@ -1,4 +1,54 @@
 import { waitForLoad, nodeListToArray } from './utils/dom';
+import storage from './utils/storage';
+import { MODULE_MAP, SECTION_MAP } from './module-map';
+
+const formatModuleName = name => {
+  let n = name.split(/(?=[A-Z])/).join(' ');
+  return n.charAt(0).toUpperCase() + n.substring(1);
+};
+
+function createOptionsSection(sectionTitle, modules, sectionHref, options) {
+  const sectionWrap = document.createElement('div');
+  const title = document.createElement('div');
+  const optionsWrap = document.createElement('div');
+
+  sectionWrap.setAttribute('data-gocp-href', sectionHref);
+  title.className = 'bb-section-heading';
+  title.innerText = sectionTitle;
+  optionsWrap.style.padding = '2px 0';
+
+  modules.forEach(({ name }) => {
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const checkbox = document.createElement('span');
+    const caption = document.createElement('span');
+
+    label.className = 'bb-check-wrapper';
+    label.setAttribute('for', name);
+    label.setAttribute('data-gocp-module', name);
+    label.style.display = 'block';
+    input.id = name;
+    input.type = 'checkbox';
+    input.checked = options[name];
+    checkbox.className = 'bb-check-checkbox';
+    caption.style.fontWeight = 'normal';
+    caption.style.marginLeft = '10px';
+    caption.innerText = formatModuleName(name);
+
+    label.appendChild(input);
+    label.appendChild(checkbox);
+    label.appendChild(caption);
+
+    optionsWrap.appendChild(label);
+
+  });
+
+  sectionWrap.appendChild(title);
+  sectionWrap.appendChild(optionsWrap);
+
+  return sectionWrap;
+}
+
 function generateDialogHtml() {
   return `
     <div class="modal bb-modal in" id="gocp_options_modal" style="display: block;" tabindex="-1">
@@ -48,6 +98,17 @@ const closeModal = e => {
   }
   window.location.reload();
 };
+const loadOptions = async () => {
+  for (let i in MODULE_MAP) {
+    // created elem-by-elem because embedding HTML will cause event listeners not to work
+    if ({}.hasOwnProperty.call(MODULE_MAP, i)) {
+      const options = await storage.get('options');
+      let section = createOptionsSection(SECTION_MAP[i], MODULE_MAP[i], i, options[i]);
+      document.getElementById('gocp_options_sections').appendChild(section);
+    }
+  }
+};
+
 async function constructDialog() {
   document.body.innerHTML += generateDialogHtml();
   loadOptions();
