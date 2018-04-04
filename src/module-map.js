3f8e02ec6073c4b27c60fd93f699791a517b429a
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-04-04 18:22:35 -0400

    Migrate module listing to new format

---
+++ src/module-map.js
@@ -0,0 +1,29 @@
+import autoCloseDetailStatus from './modules/auto-close-detail-status';
+import inlineChangeStatus from './modules/inline-change-status';
+import toggleCompleted from './modules/toggle-completed';
+import highlightCurrentClass from './modules/highlight-current-class';
+import freeBlock from './modules/free-block';
+import gradeSummary from './modules/grade-summary';
+import archiveAll from './modules/archive-all';
+
+/* eslint-disable quote-props */
+
+const MODULE_MAP = {
+  'assignmentdetail': [autoCloseDetailStatus],
+  'studentmyday/assignment-center': [inlineChangeStatus, toggleCompleted],
+  'studentmyday/schedule': [highlightCurrentClass, freeBlock],
+  'studentmyday/progress': [gradeSummary],
+  'message': [archiveAll],
+};
+
+const SECTION_MAP = {
+  'assignmentdetail': 'Assignment Detail',
+  'studentmyday/assignment-center': 'Assignment Center',
+  'studentmyday/schedule': 'Schedule',
+  'studentmyday/progress': 'Progress',
+  'message': 'Messages',
+};
+
+/* eslint-enable quote-props */
+
+export { MODULE_MAP, SECTION_MAP };
