3ba0790b1e571723c4907f38c63d70175b0d94a4
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-04-06 21:19:49 -0400

    Auto-initialize new modules in options

--- src/index.js
+++ src/index.js
@@ -12,13 +12,40 @@ async function loadModules() {
   for (let section in optsData) {
     if (window.location.hash.startsWith(`#${section}`)) {
       for (let module in optsData[section]) {
-        if (optsData[section][module]) MODULE_MAP[section].filter(s => s.name === module)[0]();
+        if (optsData[section][module]) {
+          const moduleFunc = MODULE_MAP[section].filter(s => s.name === module)[0];
+          if (moduleFunc) {
+            moduleFunc();
+          }
+        }
       }
     }
   }
 
 }
 
+async function initializeOptions() {
+  const optsObj = await storage.get('options');
+  for (let i in MODULE_MAP) {
+    if ({}.hasOwnProperty.call(MODULE_MAP, i)) {
+      if (!optsObj[i]) {
+        optsObj[i] = {};
+      }
+      for (let j in MODULE_MAP[i]) {
+        if ({}.hasOwnProperty.call(MODULE_MAP[i], j)) {
+          const { name } = MODULE_MAP[i][j];
+          if (optsObj[i][name] === undefined) optsObj[i][name] = true;
+        }
+      }
+    }
+  }
+  await storage.set({ options: optsObj });
+}
+
 window.onhashchange = loadModules;
 
 loadModules();
+
+// TODO: only call initializeOptions on install and update
+// (plus dev feature to force initialization)
+initializeOptions();
