c6b680acfe765f0224212f37e30952fc165eef32
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-06-18 21:03:09 -0400

    Support mobile classes menu in search
    Also, general clean up of search-classes-menu

--- src/modules/search-classes-menu.js
+++ src/modules/search-classes-menu.js
@@ -1,49 +1,124 @@
-import { waitForLoad, insertAfter, nodeListToArray } from '../utils/dom';
+import { waitForLoad, nodeListToArray, insertBefore } from '../utils/dom';
+import { isDesktop } from '../utils/ui';
+
 import registerModule from '../utils/module';
 
-let classes;
+const formats = {
+  DESKTOP: 'desktop',
+  MOBILE: 'mobile',
+};
+const classNames = {
+  desktopSearchbar: 'gocp_search-classes-menu_searchbar',
+  mobileSearchbar: 'gocp_search-classes-menu_mobile-searchbar',
+  desktopClassesMenu: 'subnav sec-75-bordercolor white-bgc subnav-multicol nav-visible',
+};
+
+let courses;
 let match;
+let format = isDesktop() ? formats.DESKTOP : formats.MOBILE;
+
+const getFormat = () => (isDesktop() ? formats.DESKTOP : formats.MOBILE);
+
+const getDesktopCourseElems = () => {
+  // TODO: support >2 collumns
+  const cols = document.getElementsByClassName(classNames.desktopClassesMenu)[0].children;
+  const elems = [
+    ...nodeListToArray(cols[0].children).filter(e => e.id !== classNames.desktopSearchbar),
+    ...nodeListToArray(cols[1].children),
+  ];
+  return elems.map(elem => ({
+    title: elem.innerText.toLowerCase().trim(),
+    elem,
+  }));
+};
+const getMobileCourseElems = () => {
+  const elems = document.getElementsByClassName('app-mobile-level')[2].children[2].children;
+  return nodeListToArray(elems)
+    .filter(e => e.id !== classNames.mobileSearchbar)
+    .map(elem => ({
+      title: elem.children[0].innerText.toLowerCase().trim(),
+      elem,
+    }));
+};
 
 function generateCourseList() {
-  const elems = document.getElementsByClassName('subnav sec-75-bordercolor white-bgc subnav-multicol nav-visible')[0].children;
-  return [...nodeListToArray(elems[0].children), ...nodeListToArray(elems[1].children)].filter(e => e.tagName !== 'INPUT');
+  return isDesktop() ? getDesktopCourseElems() : getMobileCourseElems();
+}
+
+function hideCourse({ elem }) {
+  elem.style.background = 'initial';
+  elem.children[0].style.background = 'initial';
+  elem.style.opacity = '0.1';
+  if (isDesktop()) {
+    const text = elem.firstChild.firstChild.firstChild;
+    text.className = 'title black-fgc';
+    text.style.color = 'black';
+  } else {
+    elem.children[0].style.textShadow = 'initial';
+    elem.children[0].style.color = 'white';
+  }
+}
+function showCourse({ elem }) {
+  elem.style.opacity = '1';
+  if (isDesktop()) {
+    elem.children[0].children[0].children[0].className = '';
+  }
+}
+function highlightCourse({ elem }) {
+  elem.children[0].style.background = isDesktop() ? '#d9edf7' : 'rgb(234, 215, 104)';
+  if (!isDesktop()) {
+    elem.children[0].style.textShadow = 'none';
+    elem.children[0].style.color = 'black';
+  }
 }
 
 function goToMatch(e) {
   if (e.key === 'Enter' && match) {
-    match.children[0].children[0].children[0].click();
+    if (isDesktop()) {
+      match.elem.children[0].children[0].children[0].click();
+    } else {
+      match.elem.children[0].click();
+    }
   }
 }
 
 function handleSearch(e) {
-  if (!classes) {
-    classes = generateCourseList();
+  if (!courses || format !== getFormat()) {
+    courses = generateCourseList();
   }
   match = null;
-  const matches = classes.filter(course => {
-    course.style.opacity = '0.1';
-    course.style.background = 'initial';
-    // course.style.fontWeight = 'normal';
-    course.children[0].children[0].children[0].className = 'title black-fgc';
-    course.children[0].children[0].children[0].style.color = 'black';
-    course.children[0].style.background = 'initial';
-    return course.innerText.toLowerCase().trim().startsWith(e.target.value.toLowerCase());
+  const matches = courses.filter(course => {
+    hideCourse(course);
+    return course.title.startsWith(e.target.value.toLowerCase());
   });
-  matches.forEach(course => {
-    course.style.opacity = '1'; // reset style
-    course.children[0].children[0].children[0].className = '';
-  });
+  matches.forEach(showCourse);
   if (matches.length === 1) {
-    matches[0].children[0].style.background = '#d9edf7';
+    highlightCourse(matches[0]);
     [match] = matches;
   }
 }
 
+function resetSearchbar(inputElem) {
+  inputElem.value = '';
+  inputElem.dispatchEvent(new Event('change'));
+  handleSearch({
+    target: { value: '' },
+  });
+  inputElem.focus();
+}
+
 function renderMobileSearchBar() {
+
+  const search = document.getElementById(classNames.mobileSearchbar);
+  if (search) {
+    return resetSearchbar(search.firstChild.firstChild);
+  }
+
   const li = document.createElement('li');
   const a = document.createElement('a');
-  a.className = 'mobile-group-page-link-1';
   const input = document.createElement('input');
+  li.id = classNames.mobileSearchbar;
+  a.className = 'mobile-group-page-link-1';
   input.placeholder = 'Search';
   input.style.background = '#880d2f';
   input.style.color = 'white';
@@ -52,27 +127,29 @@ function renderMobileSearchBar() {
   input.style.fontSize = '1.2em';
   a.appendChild(input);
   li.appendChild(a);
-  return li;
+  input.oninput = handleSearch;
+  document.getElementsByClassName('app-mobile-level')[2].children[2].prepend(li);
+  input.focus();
 }
 
-function renderSearchBar() {
+function renderDesktopSearchBar() {
 
-  const menu = document.getElementsByClassName('subnav sec-75-bordercolor white-bgc subnav-multicol nav-visible')[0];
-  const search = document.getElementById('gocp_search-classes-menu_searchbar');
+  const getClassesMenu = () => document.getElementsByClassName(classNames.desktopClassesMenu)[0];
+
+  const menu = getClassesMenu();
+  const search = document.getElementById(classNames.desktopSearchbar);
+
   if (search) {
-    search.value = '';
-    // search.dispatchEvent(new Event('change'));
-    handleSearch({
-      target: { value: '' },
-    }); // :(
-    return search.focus();
+    return resetSearchbar(search);
   }
   if (!menu) {
     return setTimeout(() => {
-      renderSearchBar();
+      renderDesktopSearchBar();
     }, 100);
   }
+
   const input = document.createElement('input');
+
   input.id = 'gocp_search-classes-menu_searchbar';
   input.className = 'filter-search-box form-control';
   input.size = '16';
@@ -80,31 +157,29 @@ function renderSearchBar() {
   input.style.width = '170px';
   input.style.height = '29.6px';
   input.style.display = 'inline';
+  input.autocomplete = 'false';
   input.placeholder = 'Filter Classes';
-  let elem = document.getElementsByClassName('subnav sec-75-bordercolor white-bgc subnav-multicol nav-visible')[0].children[0].children[0];
-  elem.parentNode.insertBefore(input, elem);
+  input.oninput = handleSearch;
+
+  const firstClass = getClassesMenu().firstChild.firstChild;
+  insertBefore(firstClass, input);
+
   input.focus();
-  document.getElementById('gocp_search-classes-menu_searchbar').oninput = handleSearch;
 }
 
 function searchClassesMenu() {
+  document.body.addEventListener('keypress', goToMatch);
+  // desktop
   waitForLoad(() => document.getElementById('group-header-Classes')).then(() => {
-    document.body.addEventListener('keypress', goToMatch);
     document.getElementsByClassName('twoline parentitem')[0].addEventListener('mouseenter', () => {
-      renderSearchBar();
+      renderDesktopSearchBar();
     });
   });
-  // waitForLoad(() => document.getElementById('site-mobile-sitenav').children[0].children.length > 1).then(() => {
-  //   document.getElementById('site-mobile-sitenav').children[0].prepend(renderMobileSearchBar());
-  // });
-
-  waitForLoad(() => document.getElementById('mobile-group-header-Classes') && document.getElementsByClassName('app-mobile-level').length).then(() => {
-    console.log(document.getElementById('mobile-group-header-Classes').parentNode);
-    document.getElementById('mobile-group-header-Classes').parentNode.addEventListener('click', e => {
-      console.log(e.target);
-      console.log(document.getElementById('mobile-group-header-Classes').parentNode);
-      alert(1);
-      document.getElementsByClassName('app-mobile-level open')[0].children[2].prepend(renderMobileSearchBar());
+  // mobile
+  waitForLoad(() => document.getElementById('mobile-group-header-Classes')).then(() => {
+    document.getElementById('mobile-group-header-Classes').addEventListener('click', () => {
+      waitForLoad(() => document.getElementsByClassName('app-mobile-level').length)
+        .then(renderMobileSearchBar);
     });
   });
 }
---
+++ src/utils/ui.js
@@ -0,0 +1,3 @@
+export function isDesktop() {
+  return window.innerWidth > 979;
+}
