41091428c8c9e75b394925fa6162ddadd2892e37
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-06-24 21:40:18 -0400

    Create assets folder

---
+++ src/utils/assets.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * Get URL of asset in assets/ folder.
+ * @param {string} assetName Asset name, relative to assets/
+ * @returns {string} Full URL of asset
+ */
+export function getAssetUrl(assetName) {
+  return chrome.runtime.getURL(`assets/${assetName}`);
+}
