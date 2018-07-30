339fe063cc5bdc067f7423c793c8110687045f43
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-07-30 21:08:16 +0300

    [audit] Clean up Improve Grades module

--- src/modules/improve-grades.js
+++ src/modules/improve-grades.js
@@ -3,8 +3,59 @@
   THIS MODULE DOES NOT ACTUALLY IMPROVE THE USER'S GRADES
 */
 
-import { waitForLoad, constructButton, nodeListToArray } from '../utils/dom';
+import { waitForLoad, constructButton, insertCss } from '../utils/dom';
 import registerModule from '../utils/module';
+
+function addStyles() {
+  insertCss(`
+    /* W3C */
+    @keyframes fun-time-awesome {
+      0% {background-position: left top;}
+      100% {background-position: left bottom;}
+    }
+    
+    /* Firefox */
+    @-moz-keyframes fun-time-awesome {
+      0% {background-position: left top;}
+      100% {background-position: left bottom;}
+    }
+    
+    /* Chrome, Safari */
+    @-webkit-keyframes fun-time-awesome {
+      0% {background-position: left top;}
+      100% {background-position: left bottom;}
+    }
+
+    #gocp_improve-grades_button {
+      color: white !important;
+      font-weight: bold;
+      margin-left: 10px;
+      margin-right: 10px;
+    
+      background: linear-gradient(to top, #ff3232 0%,#fcf528 16%,#28fc28 32%,#28fcf8 50%,#272ef9 66%,#ff28fb 82%,#ff3232 100%);
+      /* Chrome has a bug (?) where unprefixed background-size cause background-position animation to not work */
+      background-size: 1000%;
+      -moz-background-size: 1000%;
+      -webkit-background-size: 1000%;
+    
+      /* W3C */
+      animation-name: fun-time-awesome;
+      animation-duration: 40s;
+      animation-timing-function: linear;
+      animation-iteration-count: infinite;
+      animation-direction: alternate;
+      animation-play-state: running;
+    
+      /* Chrome, Safari */
+      -webkit-animation-name: fun-time-awesome;
+      -webkit-animation-duration: 1s;
+      -webkit-animation-timing-function: linear;
+      -webkit-animation-iteration-count: infinite;
+      -webkit-animation-direction: alternate;
+      -webkit-animation-play-state: running;
+    }
+  `);
+}
 
 let interval;
 function stopRapid() {
@@ -13,23 +64,25 @@ function stopRapid() {
   }
 }
 
-function generateReport(event, increaseBy) {
+function changeGrades(e, increaseBy) {
 
   document.querySelector('label[data-action="showGrade"]').click();
-  nodeListToArray(document.getElementsByClassName('showGrade')).forEach(e => {
-    const curGrade = Number(e.innerText.split('%')[0]);
+  const gradeTexts = document.getElementsByClassName('showGrade');
+  for (const gradeText of gradeTexts) {
+    const curGrade = Number(gradeText.innerText.split('%')[0]);
+    const button = document.querySelector('#gocp_improve-grades_button');
     if (curGrade < 100 || Number.isNaN(curGrade)) {
-      e.innerText = '100.00%';
-      document.getElementById('gocp_improve-grades_button').innerText = 'IMPROVE GRADES MORE';
+      gradeText.innerText = '100.00%';
+      button.innerText = 'IMPROVE GRADES MORE';
     } else if (curGrade < 300 || increaseBy) {
-      e.innerText = `${curGrade + (increaseBy || 10)}.00%`;
+      gradeText.innerText = `${curGrade + (increaseBy || 10)}.00%`;
     } else {
-      interval = setInterval(() => generateReport(e, 100), 500);
-      document.getElementById('gocp_improve-grades_button').innerText = 'RAPIDLY IMPROVE GRADES';
-      document.getElementById('gocp_improve-grades_button').removeEventListener('click', generateReport);
-      document.getElementById('gocp_improve-grades_button').addEventListener('click', stopRapid);
+      interval = setInterval(() => changeGrades(gradeText, 100), 500);
+      button.innerText = 'RAPIDLY IMPROVE GRADES';
+      button.removeEventListener('click', changeGrades);
+      button.addEventListener('click', stopRapid);
     }
-  });
+  }
 
 }
 const DOM_QUERY = () => (
@@ -45,84 +98,18 @@ function isAprilFools() {
   return date.getMonth() === 3 && date.getDate() === 1;
 }
 
-function improveGrades() {
+async function improveGrades() {
 
-  // only enable on april 1st
   if (!isAprilFools()) {
-    return;
+    // return;
   }
+  await waitForLoad(DOM_QUERY);
 
-  waitForLoad(DOM_QUERY).then(() => {
-    // TODO: investigate potential performance issues
-    const button = constructButton('IMPROVE GRADES', 'gocp_improve-grades_button', '', generateReport);
-    button.className += ' pull-right';
-    const wrap = document.getElementById('courses').children[0].children[0].children[1].children[0];
-    wrap.children[0].children[0].appendChild(button);
-    setTimeout(() => {
-      document.head.innerHTML += `
-      <style>
-      #gocp_improve-grades_button {
-        color: white !important;
-        font-weight: bold;
-        margin-right: 10px;
-      
-        /* W3C 
-        background: linear-gradient(top, #ff3232 0%,#fcf528 16%,#28fc28 32%,#28fcf8 50%,#272ef9 66%,#ff28fb 82%,#ff3232 100%);
-      
-        /* Firefox */
-        background: -moz-linear-gradient(top, #ff3232 0%, #fcf528 16%, #28fc28 32%, #28fcf8 50%, #272ef9 66%, #ff28fb 82%, #ff3232 100%);
-      
-        /* Chrome,Safari4+ */
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#ff3232), color-stop(16%,#fcf528), color-stop(32%,#28fc28), color-stop(50%,#28fcf8), color-stop(66%,#272ef9), color-stop(82%,#ff28fb), color-stop(100%,#ff3232));
-      
-        /* Chrome10+,Safari5.1+ */
-        background: -webkit-linear-gradient(top, #ff3232 0%,#fcf528 16%,#28fc28 32%,#28fcf8 50%,#272ef9 66%,#ff28fb 82%,#ff3232 100%);
-      
-        background-size: 1000%;
-        -moz-background-size: 1000%;
-        -webkit-background-size: 1000%;
-      
-        /* W3C */
-        animation-name: fun-time-awesome;
-        animation-duration: 40s;
-        animation-timing-function: linear;
-        animation-iteration-count: infinite;
-        animation-direction: alternate;
-        animation-play-state: running;
-      
-        /* Chrome, Safari */
-        -webkit-animation-name: fun-time-awesome;
-        -webkit-animation-duration: 1s;
-        -webkit-animation-timing-function: linear;
-        -webkit-animation-iteration-count: infinite;
-        -webkit-animation-direction: alternate;
-        -webkit-animation-play-state: running;
-      }
-      
-      /* W3C */
-      @keyframes fun-time-awesome {
-        0% {background-position: left top;}
-        100% {background-position: left bottom;}
-      }
-      
-      /* Firefox */
-      @-moz-keyframes fun-time-awesome {
-        0% {background-position: left top;}
-        100% {background-position: left bottom;}
-      }
-      
-      /* Chrome, Safari */
-      @-webkit-keyframes fun-time-awesome {
-        0% {background-position: left top;}
-        100% {background-position: left bottom;}
-      }
-      
-      */
-      
-      </style>
-    `;
-    });
-  }, 1000000000);
+  const button = constructButton('IMPROVE GRADES', 'gocp_improve-grades_button', '', changeGrades);
+  button.className += ' pull-right';
+  const wrap = document.getElementById('courses').children[0].children[0].children[1].children[0];
+  wrap.children[0].children[0].appendChild(button);
+  addStyles();
 }
 
 export default registerModule('Improve Grades', improveGrades, {
