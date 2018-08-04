9ffc30c1406d698e73c2f8b3d8a9555dbb663342
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-04 21:22:42 +0300

    Remove unneccessary trim

--- src/modules/inline-change-status/index.js
+++ src/modules/inline-change-status/index.js
@@ -24,9 +24,9 @@ function createOptionElem(name, val) {
 function createDropdown(parentNode, controller, index, preVal) {
   const existingValue = preVal || document.getElementsByClassName('assignment-status-update')[index].parentNode.parentNode.children[5].textContent.trim();
   const selectElem = document.createElement('select');
-  optionNames.splice(optionNames.map(n => n.trim()).indexOf(existingValue), 1);
   // oncampus natively uses non breaking spaces for ONLY in progress labels
   const optionNames = ['To Do', 'In\u00a0Progress', 'Completed'];
+  optionNames.splice(optionNames.indexOf(existingValue), 1);
 
   const optionElems = [
     createOptionElem('-- Select --', '0'),
