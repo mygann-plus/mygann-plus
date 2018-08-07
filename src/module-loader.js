18be7b1511697b79275b83690178981a64f2dbf9
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-07 16:46:49 +0300

    Create module loader public API

---
+++ src/module-loader.js
@@ -0,0 +1,43 @@
+import { getOptionsFor } from '~/options';
+
+const loadedModules = new Set();
+
+function tryRunFunction(fn) {
+  try {
+    fn();
+  } catch (e) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(e); // eslint-disable-line no-console
+    }
+  }
+}
+
+export function isModuleLoaded(module) {
+  return loadedModules.has(module);
+}
+
+export function tryLoadModule(module) {
+  const options = getOptionsFor(module.guid);
+  if (!options.enabled) {
+    return false;
+  }
+  if (!isModuleLoaded(module) && module.init) {
+    tryRunFunction(() => module.init(options));
+  }
+  if (module.main) {
+    tryRunFunction(() => module.main(options));
+  }
+  loadedModules.add(module);
+  return true;
+}
+
+export function tryUnloadModule(module) {
+  if (!isModuleLoaded(module)) {
+    return false;
+  }
+  if (!module.config.affectsGlobalState) {
+    loadedModules.delete(module);
+    return true;
+  }
+  return false;
+}
