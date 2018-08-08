6db7897784887fae3c332ab84752c3093d44e331
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-08 17:38:32 +0300

    Add support for dynamic reloading in Day Schedule Button

--- src/modules/day-schedule-button.js
+++ src/modules/day-schedule-button.js
@@ -7,11 +7,10 @@ const getMonthButton = () => (
 
 const goToDay = () => { window.location.hash = 'studentmyday/schedule'; };
 
-async function dayScheduleButton() {
+async function dayScheduleButton(opts, unloaderContext) {
   await waitForLoad(getMonthButton);
 
   const monthButton = getMonthButton();
-
 
   const dayButton = (
     <span className="fc-button fc-corner-left fc-state-default" onClick={ goToDay }>
@@ -26,10 +25,17 @@ async function dayScheduleButton() {
 
   monthButton.classList.remove('fc-corner-left');
   monthButton.before(dayButton);
+
+  unloaderContext.addRemovable(dayButton);
+}
+
+function unloadDayScheduleButton() {
+  getMonthButton().classList.add('fc-corner-left');
 }
 
 export default createModule('{0ae24306-a117-447f-94e1-9a296d2b8a7d}', {
   name: 'Day Schedule Button',
   main: dayScheduleButton,
+  unload: unloadDayScheduleButton,
   description: 'Button to return to day view',
 });
