47c1d2f63eb430213b995c62c80f8e47b3b075d8
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-08 18:13:24 +0300

    Add support for dynamic reloading in Filter Website Main Search

--- src/modules/filter-website-main-search.js
+++ src/modules/filter-website-main-search.js
@@ -1,6 +1,16 @@
 import createModule from '~/utils/module';
 import { waitForLoad } from '~/utils/dom';
 
+const selectors = {
+  pagesResults: 'gocp_filter-website-main-search_pages-results',
+};
+
+const hidingTypes = {
+  collapse: 'collapse',
+  moveToBottom: 'moveToBottom',
+  remove: 'remove',
+};
+
 async function filterWebsiteMainSearch(options) {
   await waitForLoad(() => document.querySelector('.bb-tile-title'));
 
@@ -11,33 +21,62 @@ async function filterWebsiteMainSearch(options) {
   if (title.dataset.target !== '#searchSummaryPages') {
     return;
   }
+  elem.classList.add(selectors.pagesResults);
 
   switch (options.hidingType) {
-    case 'Collapse':
+    case hidingTypes.collapse:
     default:
-      elem.children[1].style.height = '0px';
+      elem.children[1].style.height = '0px'; // prevents closing animation
       title.click();
       break;
-    case 'Move To Bottom':
+    case hidingTypes.moveToBottom:
       parent.appendChild(elem);
       break;
-    case 'Remove':
-      elem.remove();
+    case hidingTypes.remove:
+      elem.style.display = 'none';
   }
 
+}
+
+function unloadFilterWebsiteMainSearch(options) {
+  const elem = document.querySelector(`.${selectors.pagesResults}`);
+  const parent = document.querySelector('#search_summary_results');
+
+  if (!elem) {
+    return;
+  }
+
+  switch (options.hidingType) {
+    case hidingTypes.collapse:
+      document.querySelector('.bb-tile-title').click();
+      break;
+    case hidingTypes.moveToBottom:
+      parent.prepend(elem);
+      break;
+    case hidingTypes.remove:
+      elem.style.display = '';
+      break;
+    default:
+      break;
+  }
 }
 
 export default createModule('{d560bad4-1073-4452-ac11-f4466dc19184}', {
   name: 'Filter Website from Search',
   description: 'Hide results from the Gann Website in OnCampus searches',
   main: filterWebsiteMainSearch,
+  unload: unloadFilterWebsiteMainSearch,
   defaultEnabled: false,
   suboptions: {
     hidingType: {
       name: 'Hiding Type',
       type: 'enum',
-      defaultValue: 'Collapse',
-      enumValues: ['Collapse', 'Move To Bottom', 'Remove'],
+      defaultValue: hidingTypes.collapse,
+      enumValues: {
+        [hidingTypes.collapse]: 'Collapse',
+        [hidingTypes.moveToBottom]: 'Move To Bottom',
+        [hidingTypes.remove]: 'Remove',
+      },
     },
   },
 });
