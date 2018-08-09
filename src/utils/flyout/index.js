39fb7878be43a49deeb89a38db73309968c51b28
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-09 20:05:11 +0300

    Create Flyout utility

---
+++ src/utils/flyout/index.js
@@ -0,0 +1,80 @@
+import classNames from 'classnames';
+
+import { createElement, insertCss } from '~/utils/dom';
+
+import style from './style.css';
+
+const selectors = {
+  flyout: style.locals.flyout,
+  arrow: style.locals.arrow,
+};
+
+export default class Flyout {
+
+  constructor(innerElem, opts) {
+    const defaultOpts = {
+      autoHide: true,
+      onHide: () => {},
+    };
+
+    this.opts = { ...defaultOpts, ...opts };
+
+    this.innerElem = innerElem;
+    this._generateOuterElem();
+  }
+
+  showAtElem(targetElem) {
+    const { bottom, left, width } = targetElem.getBoundingClientRect();
+    const offsetBottom = window.scrollY + bottom;
+    const offsetLeft = window.scrollX + left;
+    this.showAt(offsetLeft + (width / 2), offsetBottom);
+  }
+
+  showAt(x, y) {
+    this.outerElem.style.left = `${x}px`;
+    this.outerElem.style.top = `${y}px`;
+    document.body.appendChild(this.outerElem);
+
+    setTimeout(() => {
+      this.outerElem.classList.add('in');
+    }, 0);
+
+    if (this.opts.autoHide) {
+      document.addEventListener('mousedown', () => this.hide(), {
+        once: true,
+      });
+    }
+  }
+
+  hide() {
+    this.opts.onHide();
+    this.outerElem.classList.remove('in');
+    this.outerElem.addEventListener('transitionend', () => {
+      this.outerElem.remove();
+    }, { once: true });
+  }
+
+  getBody() {
+    return this.outerElem.querySelector('.popover-content > :first-child');
+  }
+
+  _generateOuterElem() {
+    this.outerElem = (
+      <div
+        className={classNames('popover fade bottom', selectors.flyout)}
+        role="tooltip"
+        onMouseDown={e => e.stopPropagation()}
+      >
+        <div className={classNames('arrow', selectors.arrow)}></div>
+        <div className="popover-content">
+          <div>
+            { this.innerElem }
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+insertCss(style.toString());
