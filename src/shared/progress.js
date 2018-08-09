be733565d1801b3945a355cb77510ccf5dc8fe01
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-09 18:02:43 +0300

    Create observeCourseBar in shared progress

--- src/shared/progress.js
+++ src/shared/progress.js
@@ -1,9 +1,16 @@
-/* eslint-disable import/prefer-default-export */
-
 export function coursesListLoaded() {
   return document.querySelector('#coursesContainer > *') &&
     document.querySelectorAll('.bb-tile-content-section')[3] &&
     document.querySelectorAll('.bb-tile-content-section')[3].children[0];
 }
 
-/* eslint-enable import/prefere-default-export */
+export function observeCoursesBar(fn) {
+  const courseWrap = document.querySelector('#coursesCollapse').closest('.ch');
+  const observer = new MutationObserver(fn);
+  observer.observe(courseWrap, {
+    childList: true,
+  });
+  return {
+    remove() { observer.disconnect(); },
+  };
+}
