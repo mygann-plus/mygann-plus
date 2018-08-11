a2673f661edddf9c4eb54936376c4e59e5233ee2
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-11 22:12:21 +0300

    Extract favorites dialogs into seperate file

--- src/modules/favorites/favorites-ui.js
+++ src/modules/favorites/favorites-ui.js
@@ -1,115 +1,12 @@
 import classNames from 'classnames';
 
 import { getAssetUrl } from '~/utils/assets';
-import Dialog from '~/utils/dialog';
 import Flyout from '~/utils/flyout';
-import { createElement, constructButton, clearNode } from '~/utils/dom';
+import { createElement, clearNode, constructButton } from '~/utils/dom';
 
-import {
-  editSavedFavorite,
-  getFavorite,
-  deleteSavedFavorite,
-  saveNewFavorite,
-} from './favorites-model';
 import selectors from './selectors';
-
-function getHash() {
-  return window.location.hash.split('#')[1] || '';
-}
-
-function createDialogBody({ hash = getHash(), title = '' }) {
-  return (
-    <form>
-      <div className="row">
-        <div className="form-group col-md-12">
-          <label className="control-label" htmlFor={selectors.dialog.title}>Title</label>
-          <div className="controls">
-            <input
-              type="text"
-              className="form-control col-md-8"
-              name={selectors.dialog.title}
-              id={selectors.dialog.title}
-              value={title}
-              required
-              autocomplete="off"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="form-group col-md-12">
-          <label className="control-label" htmlFor={selectors.dialog.hash}>Page</label>
-          <div className="controls">
-            <input
-              type="text"
-              className="form-control col-md-8"
-              name={selectors.dialog.hash}
-              id={selectors.dialog.hash}
-              value={hash}
-              required
-            />
-          </div>
-        </div>
-      </div>
-    </form>
-  );
-}
-
-function showDialog(dialogTitle, primaryButtonName, state = {}) {
-  return new Promise(res => {
-    const form = createDialogBody(state);
-    const dialog = new Dialog(dialogTitle, form, {
-      leftButtons: [
-        {
-          name: primaryButtonName,
-          primary: true,
-          onClick() {
-            if (!form.reportValidity()) {
-              // prevent dialog from closin
-              return false;
-            }
-            const title = form.elements[selectors.dialog.title].value;
-            const hash = form.elements[selectors.dialog.hash].value;
-            res({ title, hash });
-          },
-        },
-        {
-          type: Dialog.buttonTypes.LINK,
-          name: 'Cancel',
-          onClick() { res(null); },
-        },
-      ],
-    });
-    dialog.open();
-    form.elements[selectors.dialog.title].focus();
-  });
-}
-
-async function handleAdd(event) {
-  event.preventDefault();
-
-  const newFavorite = await showDialog('Add Favorite', 'Add');
-  if (!newFavorite) {
-    return;
-  }
-  await saveNewFavorite(newFavorite);
-}
-
-
-async function handleEdit(event, id) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  const oldFavorite = await getFavorite(id);
-  const newFavorite = await showDialog('Edit Favorite', 'Save', oldFavorite);
-  if (!newFavorite) {
-    return;
-  }
-  await editSavedFavorite(id, {
-    ...newFavorite,
-    id,
-  });
-}
+import { handleAdd, handleEdit } from './favorites-dialogs';
+import { deleteSavedFavorite } from './favorites-model';
 
 function handleDelete(event, id) {
   event.preventDefault();
@@ -134,6 +31,7 @@ function handleDelete(event, id) {
   flyout.getBody().focus();
 }
 
+
 function createLink(favorite) {
   return (
     <li className={selectors.menuItem.link}>
@@ -155,7 +53,6 @@ function createLink(favorite) {
     </li>
   );
 }
-
 
 export function createMenu() {
   const starIconUrl = getAssetUrl('star_icon.png');
