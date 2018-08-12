32c6f971c5c511cda16679099480ca7141c6a386
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-12 22:34:34 +0300

    Remove useless BabelMinifyPlugin
    Webpack production mode minifies javascript automatically

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
-
 module.exports = {
   extends: require.resolve('./webpack.common'),
   mode: 'production',
-  plugins: [
-    new BabelMinifyPlugin(),
-  ],
 };
