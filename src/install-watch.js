52f47a12bc43cdbf618924027764cf3eeb799f50
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-08-20 17:10:38 +0300

    Only display update indicator on non-patch updates

--- src/install-watch.js
+++ src/install-watch.js
@@ -1,5 +1,17 @@
-import { markInstallState } from '~/install';
+import { markInstallState, installStates } from '~/install';
 
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  markInstallState(reason);
+function isPatch(prevVersion, curVersion) {
+  const [prevMajor, prevMinor, prevPatch] = prevVersion.split('.');
+  const [curMajor, curMinor, curPatch] = curVersion.split('.');
+  return prevMajor === curMajor && prevMinor === curMinor && prevPatch !== curPatch;
+}
+
+chrome.runtime.onInstalled.addListener(({ previousVersion, reason }) => {
+  if (reason === 'update') {
+    const currentVersion = chrome.runtime.getManifest().version;
+    const isUpdate = !isPatch(previousVersion, currentVersion);
+    markInstallState(isUpdate ? installStates.UPDATE : installStates.PATCH);
+  } else {
+    markInstallState(installStates.INSTALL);
+  }
 });
--- src/install.js
+++ src/install.js
@@ -3,16 +3,22 @@ import storage from '~/utils/storage';
 const INSTALL_KEY = 'install';
 const SCHEMA_VERSION = 1;
 
-export function markInstallState(reason) {
-  return storage.set(INSTALL_KEY, reason, SCHEMA_VERSION);
+export const installStates = {
+  INSTALL: 'install',
+  UPDATE: 'update',
+  PATCH: 'patch', // patch is currently unused, but still set
+};
+
+export function markInstallState(state) {
+  return storage.set(INSTALL_KEY, state, SCHEMA_VERSION);
 }
 export async function hasInstalled() {
   const installState = await storage.get(INSTALL_KEY, SCHEMA_VERSION);
-  return installState === 'install';
+  return installState === installStates.INSTALL;
 }
 export async function hasUpdated() {
   const installState = await storage.get(INSTALL_KEY, SCHEMA_VERSION);
-  return installState === 'update';
+  return installState === installStates.UPDATE;
 }
 export function clearInstallState() {
   return markInstallState('');
