6033cb6c9e1baa1fc7e4c45a9f0860c8963718db
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-09-03 11:49:18 -0400

    Support Samsung fingerprint authentication in One Click Login

--- src/modules/one-click-login.js
+++ src/modules/one-click-login.js
@@ -1,20 +1,36 @@
 import registerModule from '~/module';
 import { waitForLoad } from '~/utils/dom';
 
+const getUsernameField = () => document.getElementById('Username');
 const getPasswordDiv = () => document.getElementById('divPassword');
 const getNextbtn = () => document.getElementById('nextBtn');
+
+async function enableUsernameField() {
+  await waitForLoad(() => document.getElementById('site-login-alert').children.length);
+  getUsernameField().disabled = false;
+}
 
 async function oneClickLogin() {
   await waitForLoad(() => getPasswordDiv() && getNextbtn());
 
   getPasswordDiv().style.display = 'block';
-  getNextbtn().value = 'Login';
+  getNextbtn().style.display = 'none';
+  document.getElementById('areaLogin').style.display = 'block';
 
-  document.getElementById('nextBtn').addEventListener('click', async () => {
-    document.getElementById('loginBtn').click();
-    await waitForLoad(() => document.getElementById('site-login-alert').children.length);
-    document.getElementById('Username').disabled = false;
+  document.getElementById('loginBtn').addEventListener('click', async () => {
+    enableUsernameField();
   });
+  getNextbtn().addEventListener('click', async () => {
+    document.getElementById('loginBtn').click();
+    enableUsernameField();
+  });
+  getUsernameField().addEventListener('input', e => {
+    if (e.key === 'Enter') {
+      document.getElementById('loginBtn').click();
+      enableUsernameField();
+    }
+  });
+
 }
 
 export default registerModule('{6ca82534-e670-490a-8ce1-1d87f48c7c32}', {
