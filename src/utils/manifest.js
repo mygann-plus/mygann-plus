c40f922b429a69d6d2da2beec913a452badfa1c6
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-09-07 08:12:31 -0400

    Create getManifest utility for bookmarklet support

---
+++ src/utils/manifest.js
@@ -0,0 +1,12 @@
+import { isBookmarklet } from '~/utils/bookmarklet';
+
+export default function getManifest() {
+  if (isBookmarklet()) {
+    return {
+      version_name: '0.5.0-beta',
+      description: 'A collection of modules that improve your Gann OnCampus experience',
+    };
+  } else {
+    return chrome.runtime.getManifest();
+  }
+}
