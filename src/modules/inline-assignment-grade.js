fa196300d3f9ef8df64065e2c8aaa6f0da1d0787
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-09-26 16:24:50 -0400

    Create individual inline assignment grade buttons

--- src/modules/inline-assignment-grade.js
+++ src/modules/inline-assignment-grade.js
@@ -1,9 +1,13 @@
 import registerModule from '~/module';
 
-import { waitForLoad, constructButton } from '~/utils/dom';
+import { createElement, waitForLoad, constructButton } from '~/utils/dom';
 import { fetchApi } from '~/utils/fetch';
 import { getUserId } from '~/utils/user';
-import { appendMobileAssignmentCenterMenuLink } from '~/shared/assignments-center';
+
+import {
+  appendMobileAssignmentCenterMenuLink,
+  addAssignmentTableMutationObserver,
+} from '~/shared/assignments-center';
 
 const filterUngraded = label => {
   return label.textContent === 'Graded';
@@ -21,18 +25,66 @@ async function getGrade(id) {
   const studentId = await getUserId();
   const endpoint = '/api/datadirect/AssignmentStudentDetail';
   const query = `?format=json&studentId=${studentId}&AssignmentIndexId=${id}`;
-  return fetchApi(endpoint + query);
+  const [gradeData] = await fetchApi(endpoint + query);
+  return calculateGrade(gradeData);
 }
 
-async function showGrades() {
+class InlineGrade {
+
+  constructor(label) {
+    this.label = label;
+    this.id = getIdFromLabel(label);
+    this.grade = null;
+
+    this.gradeLabel = <span></span>;
+    this.button = (
+      <i
+        className="fa fa-eye"
+        onClick={ () => this.show() }
+        style={{ marginLeft: '10px' }}
+      />
+    );
+
+    this.label.appendChild(this.gradeLabel);
+    this.label.appendChild(this.button);
+  }
+
+  async show() {
+    if (!this.grade) {
+      this.grade = await getGrade(this.id);
+      this.gradeLabel.textContent = `: ${this.grade}%`;
+    }
+    this.gradeLabel.style.display = 'inline-block';
+    this.button.style.display = 'none';
+  }
+
+  remove() {
+    this.gradeLabel.remove();
+    this.button.remove();
+  }
+
+}
+
+async function showGrades(inlineGrades) {
+  for (const inlineGrade of inlineGrades) {
+    inlineGrade.show();
+  }
+}
+
+function insertInlineGrades(unloaderContext) {
+  const inlineGrades = [];
   const gradedLabels = Array.from(document.querySelectorAll('.label-success'))
     .filter(filterUngraded);
-  const ids = gradedLabels.map(getIdFromLabel);
-  const data = await Promise.all(ids.map(getGrade));
-  const grades = data.map(([d]) => calculateGrade(d));
-  gradedLabels.forEach((label, i) => {
-    label.textContent += `: ${grades[i]}`;
-  });
+
+  for (const label of gradedLabels) {
+    const inlineGrade = new InlineGrade(label);
+    inlineGrades.push(inlineGrade);
+    unloaderContext.addRemovable(inlineGrade);
+  }
+
+  return inlineGrades;
+}
+
 }
 
 const domQuery = () => document.querySelector('#filter-status');
@@ -40,17 +92,31 @@ const domQuery = () => document.querySelector('#filter-status');
 async function inlineAssignmentGrade(opts, unloaderContext) {
   const filterStatusButton = await waitForLoad(domQuery);
 
+  let inlineGrades = [];
+
   const showGradesBtn = constructButton(
     'Show Grades',
     'gocp_inline-assignment-grade_button',
     'fa fa-eye',
-    showGrades,
+    () => showGrades(inlineGrades),
   );
   filterStatusButton.parentNode.appendChild(showGradesBtn);
-  const showGradesLink = appendMobileAssignmentCenterMenuLink('Show Grades', showGrades, 0);
+
+  const showGradesLink = appendMobileAssignmentCenterMenuLink(
+    'Show Grades',
+    () => showGrades(inlineGrades),
+    0,
+  );
+
+  inlineGrades = insertInlineGrades(unloaderContext);
+
+  const observer = await addAssignmentTableMutationObserver(() => {
+    inlineGrades = insertInlineGrades(unloaderContext);
+  });
 
   unloaderContext.addRemovable(showGradesBtn);
   unloaderContext.addRemovable(showGradesLink);
+  unloaderContext.addRemovable(observer);
 }
 
 export default registerModule('{0540d147-af76-4f44-a23d-415506e8e777}', {
