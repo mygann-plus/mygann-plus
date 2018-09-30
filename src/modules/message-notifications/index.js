8be940819b81fda28de005268646a310c6697940
Matan Kotler-Berkowitz <205matan@gmail.com>
2018-09-30 10:15:28 -0400

    Replace <br> globally in message notification text

--- src/modules/message-notifications/index.js
+++ src/modules/message-notifications/index.js
@@ -26,7 +26,7 @@ const identifiers = {
 const formatBodyText = text => {
   return text
     .replace(/\s+/g, ' ')
-    .replace(/<br>/, ' ')
+    .replace(/<br>/g, ' ')
     .trim();
 };
 
@@ -166,7 +166,9 @@ async function getMessages() {
     const data = await fetchApi('/api/message/inbox/?format=json');
     return data
       .map(conversation => {
-        return conversation.Messages.filter(message => !message.ReadInd)[0];
+        // return conversation.Messages.filter(message => !message.ReadInd)[0];
+        console.log(conversation);
+        return conversation.Messages[0];
       })
       .filter(Boolean)
       .map(conversation => ({
