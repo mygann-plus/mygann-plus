23d1745d15a66a0680d3a1e14d69dc18c7fc2de7
Matan Kotler-Berkowitz <205matan@gmail.com>
2019-08-24 22:24:29 -0400

    Clean up getDesktopCourses

--- src/shared/classes-menu.js
+++ src/shared/classes-menu.js
@@ -5,23 +5,15 @@ export const getDesktopMenu = () => (
 );
 
 export const getMobileMenu = () => (
-  document.querySelectorAll('.app-mobile-level')[2] &&
-  document.querySelectorAll('.app-mobile-level')[2].children[2]
+  document.querySelectorAll('.app-mobile-level')[2]
+  && document.querySelectorAll('.app-mobile-level')[2].children[2]
 );
 
 export function getDesktopCourses() {
-  const cols = getDesktopMenu().children;
-  const elems = [];
-  for (const col of cols) {
-    if (col.matches('.subnavfooter')) {
-      continue;
-    }
-    elems.push(...Array.from(col.children).filter(child => {
-      return !child.matches('.divider');
-    }));
-  }
+  const courses = Array.from(getDesktopMenu().querySelectorAll('li'))
+    .filter(course => !course.closest('.subnavfooter'));
 
-  return elems.map(elem => ({
+  return courses.map(elem => ({
     title: elem.textContent.toLowerCase().trim(),
     elem,
   }));
