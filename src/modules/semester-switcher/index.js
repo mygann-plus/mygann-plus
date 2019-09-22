d798542e8a10512119d585b41ae1c271b1536984
Matan Kotler-Berkowitz <205matan@gmail.com>
2019-09-22 19:36:10 -0400

    Add season switch button to Semester Switcher

--- src/modules/semester-switcher/index.js
+++ src/modules/semester-switcher/index.js
@@ -4,56 +4,96 @@ import registerModule from '~/module';
 import { waitForLoad, constructButton, insertCss } from '~/utils/dom';
 
 import style from './style.css';
-import { observeCoursesBar } from '~/shared/progress';
+import { observeCoursesBar, observeActivitiesBar } from '~/shared/progress';
 
 const selectors = {
   switchIcon: style.locals['switch-icon'],
 };
 
 const domQuery = {
-  semesterButton: value => document.querySelector(`[data-action="term"][data-value="${value}"]`),
-  secondSemester: () => document.querySelector('[data-action="term"]:last-child'),
-  activeSemester: () => document.querySelector('[data-action="term"].active'),
-  inactiveSemester: () => document.querySelector('[data-action="term"]:not(.active)'),
+  coursesBar: () => document.querySelector('#coursesCollapse'),
+  activitiesBar: () => document.querySelector('#activitiesCollapse'),
+
+  termButton: (parent, value) => parent.querySelector(`[data-action="term"][data-value="${value}"]`),
+  firstTerm: parent => parent.querySelector('[data-action="term"]:first-child'),
+  lastTerm: parent => parent.querySelector('[data-action="term"]:last-child'),
+  activeTerm: parent => parent.querySelector('[data-action="term"].active'),
+  inactiveTerm: parent => parent.querySelector('[data-action="term"]:not(.active)'),
 };
 
-async function switchSemesters() {
-  const active = domQuery.activeSemester();
-  const inactive = domQuery.inactiveSemester();
+async function switchSemesters(parent, getParent) {
+  const active = domQuery.activeTerm(parent);
+  const inactive = domQuery.inactiveTerm(parent);
+
+  // neither term or both terms are selected
+  if (!active || !inactive) {
+    const firstTerm = domQuery.firstTerm(parent);
+    return firstTerm.click();
+  }
+
   active.click();
 
   // courses bar is rerendered when active is clicked,
   // so "new" inactive is found by old inactive's data-value
-  const newInactive = await waitForLoad(() => domQuery.semesterButton(inactive.dataset.value));
+  const newInactive = await waitForLoad(() => {
+    return domQuery.termButton(getParent(), inactive.dataset.value);
+  });
   newInactive.click();
 }
 
-async function insertSwitchButton() {
-  const secondSemesterBtn = await waitForLoad(domQuery.secondSemester);
-
+function generateSwitchButton(sectionName, cb) {
   const switchButton = constructButton(
     '', '',
     classNames('fa fa-exchange', selectors.switchIcon),
-    switchSemesters,
+    cb,
   );
-  switchButton.title = 'Switch Semester';
+  switchButton.title = `Switch ${sectionName}`;
+  return switchButton;
+}
+
+async function insertSwitchButton(getParent, sectionName) {
+  const parent = getParent();
+  const secondSemesterBtn = await waitForLoad(() => domQuery.lastTerm(parent));
+
+  const switchButton = generateSwitchButton(sectionName, () => switchSemesters(parent, getParent));
 
   secondSemesterBtn.after(switchButton);
   return switchButton;
+}
+
+function insertSemesterSwitchButton() {
+  return insertSwitchButton(domQuery.coursesBar, 'Semester');
+}
+
+function insertSeasonSwitchButton() {
+  return insertSwitchButton(domQuery.activitiesBar, 'Season');
 }
 
 async function semesterSwitcher(opts, unloaderContext) {
   const styles = insertCss(style.toString());
   unloaderContext.addRemovable(styles);
 
-  let switchButtonUnloader = unloaderContext.addRemovable(await insertSwitchButton());
+  await waitForLoad(() => domQuery.coursesBar() && domQuery.activitiesBar());
+
+  let semesterSwitchButtonUnloader = unloaderContext.addRemovable(() => (
+    insertSemesterSwitchButton()
+  ));
+  let seasonSwitchButtonUnloader = unloaderContext.addRemovable(() => (
+    insertSeasonSwitchButton()
+  ));
 
   const coursesBarObserver = await observeCoursesBar(async () => {
-    switchButtonUnloader.remove();
-    switchButtonUnloader = unloaderContext.addRemovable(await insertSwitchButton());
+    semesterSwitchButtonUnloader.remove();
+    semesterSwitchButtonUnloader = unloaderContext.addRemovable(await insertSemesterSwitchButton());
+  });
+
+  const activitiesBarObserver = await observeActivitiesBar(async () => {
+    seasonSwitchButtonUnloader.remove();
+    seasonSwitchButtonUnloader = unloaderContext.addRemovable(await insertSeasonSwitchButton());
   });
 
   unloaderContext.addRemovable(coursesBarObserver);
+  unloaderContext.addRemovable(activitiesBarObserver);
 }
 
 export default registerModule('{9d28ed37-4d06-4ce5-bc25-f10776491e96}', {
