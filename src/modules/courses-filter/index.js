10aa10fbf3ddc777fdfdca43ddf5404f759947c7
Matan Kotler-Berkowitz <205matan@gmail.com>
2020-02-10 13:25:25 -0500

    Remove dropdown from courses filter
    Hide ungraded has been replaced with hide nonacademic

--- src/modules/courses-filter/index.js
+++ src/modules/courses-filter/index.js
@@ -3,18 +3,14 @@ import classNames from 'classnames';
 import registerModule from '~/module';
 
 import fuzzyMatch from '~/utils/search';
-import constants from '~/utils/style-constants';
 import { createElement, waitForLoad, insertCss } from '~/utils/dom';
 import { coursesListLoaded, observeCoursesBar } from '~/shared/progress';
 
 import style from './style.css';
 
-const CHECKED_ATTR = 'data-gocp-courses_filter-checked';
-
 const selectors = {
   hidden: style.locals.hidden,
   filterInput: style.locals['filter-input'],
-  dropdownButton: style.locals['dropdown-button'],
 };
 
 let courses;
@@ -38,7 +34,6 @@ function regenerateCoursesList() {
 function runFilter() {
   regenerateCoursesList();
   const kept = filters.reduce((arr, filter) => arr.filter(filter), courses);
-  // [audit] perhaps use dataset
   courses.forEach(course => course.elem.classList.add(selectors.hidden));
   kept.forEach(course => course.elem.classList.remove(selectors.hidden));
 }
@@ -49,72 +44,19 @@ function handleSearch(course) {
 }
 filters.push(handleSearch);
 
-function generateDropdown(items) {
-
-  const wrap = <ul className="dropdown-menu" role="menu" />;
-
-  items.forEach(item => {
-    const a = <a href="#">{ item.name }</a>;
-    const li = <li>{ a } </li>;
-
-    li.setAttribute(CHECKED_ATTR, 'false');
-
-    wrap.appendChild(li);
-
-    a.addEventListener('mousedown', e => {
-      e.preventDefault();
-      e.stopPropagation();
-      const newChecked = li.getAttribute(CHECKED_ATTR) !== 'true';
-      li.setAttribute(CHECKED_ATTR, String(newChecked));
-      li.className = newChecked ? 'active' : '';
-      runFilter();
-
-      const anyChecked = document.querySelectorAll(`li[${CHECKED_ATTR}="true"]`).length > 0;
-      const filterButton = document.querySelector(`.${selectors.dropdownButton}`);
-      filterButton.style.background = anyChecked ? constants.successGreen : 'white';
-      filterButton.children[0].style.color = anyChecked ? 'white' : 'black';
-    });
-
-    a.addEventListener('click', e => e.preventDefault());
-
-    filters.push(course => {
-      const checked = li.getAttribute(CHECKED_ATTR) === 'true';
-      return checked ? item.filter(course) : true;
-    });
-  });
-
-  return wrap;
-}
-
-function getDropdownMenu() {
-  return document.querySelector(`.${selectors.dropdownButton}`).parentNode.children[0];
-}
-function hideDropdownMenu() {
-  getDropdownMenu().style.display = 'none';
-}
-function showDropdownMenu() {
-  getDropdownMenu().style.display = 'block';
-  document.addEventListener('mousedown', hideDropdownMenu, {
-    once: true,
-  });
-}
-function toggleDropdownMenu() {
-  const wasShown = getDropdownMenu().style.display === 'block';
-  if (wasShown) {
-    hideDropdownMenu();
-  } else {
-    showDropdownMenu();
+function handleKeyDown(e) {
+  if (e.key === 'Enter') {
+    regenerateCoursesList();
+    const visible = courses.filter(({ elem }) => !elem.classList.contains(selectors.hidden));
+    if (visible.length === 1) {
+      const { elem } = visible[0];
+      const gradeDetailButton = elem.querySelector('.showGrade + .btn');
+      gradeDetailButton.click();
+    }
   }
 }
 
 function renderFilterBar() {
-
-  const dropdownFilters = [
-    {
-      name: 'Hide Ungraded Courses',
-      filter: course => course.grade !== '--',
-    },
-  ];
 
   const input = (
     <input
@@ -123,25 +65,13 @@ function renderFilterBar() {
       placeholder="Search Courses"
       autocomplete="off"
       onInput={runFilter}
+      onKeyDown={handleKeyDown}
     />
-  );
-
-  const dropdownButton = (
-    <button
-      className={ classNames('btn btn-default btn-sm dropdown-toggle', selectors.dropdownButton) }
-      dataset={{ toggle: 'dropdown' }}
-      onClick={ toggleDropdownMenu }
-      onMouseDown={ e => e.stopPropagation() }
-    >
-      <i className="fa fa-ellipsis-h" />
-    </button>
   );
 
   const wrap = (
     <div className="btn-group" style={{ display: 'inline' }}>
-      { generateDropdown(dropdownFilters) }
       { input }
-      { dropdownButton }
     </div>
   );
 
@@ -187,7 +117,7 @@ function unloadCoursesFilter() {
 
 export default registerModule('{e2c18d75-5264-4177-97b0-5c6d65fb1496}', {
   name: 'Courses Filter',
-  description: 'Search courses and hide ungraded ones.',
+  description: 'Search courses in Progress.',
   main: coursesFilter,
   unload: unloadCoursesFilter,
 });
