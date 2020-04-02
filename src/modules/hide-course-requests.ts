819bbb4d530cf3bec396e925eecab4f0113bd77f
quokkasAreTheBest <45802154+quokkasAreTheBest@users.noreply.github.com>
2020-04-02 13:46:50 -0400

    Update hide-course-requests.ts

--- src/modules/hide-course-requests.ts
+++ src/modules/hide-course-requests.ts
@@ -1,10 +1,10 @@
 import registerModule from '~/core/module';
-import { unloaderContext } from '~/core/module-loader';
 
-import { waitForLoad } from 
+import { waitForLoad } from '~/utils/dom';
 
 const courseRequestsButton = () => document.querySelector('#course-requests-btn');
 
-export default registerModule('{683a9158-9030-4015-bca6-3513b21caf72}') {
-
-}
+export default registerModule('{683a9158-9030-4015-bca6-3513b21caf72}', {
+  name: 'Hide Course Requests Tab',
+  description: 'Hide course requests tab until requests are opened'
+});
