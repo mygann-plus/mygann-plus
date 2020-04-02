46128812993318b3acd1dcde11b6e569b6ff3143
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-04-02 12:15:09 -0400

    Co-authored-by: quokkasAreTheBest <hirshsamuel48@gmail.com>

---
+++ src/modules/hide-course-requests.ts
@@ -0,0 +1,10 @@
+import registerModule from '~/core/module';
+import { unloaderContext } from '~/core/module-loader';
+
+import { waitForLoad } from 
+
+const courseRequestsButton = () => document.querySelector('#course-requests-btn');
+
+export default registerModule('{683a9158-9030-4015-bca6-3513b21caf72}') {
+
+}
