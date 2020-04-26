9ea332a822e5e104537b472ac7a01768825ec721
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-04-26 18:15:51 -0400

    hide-course-requests works!

--- src/modules/hide-course-requests.ts
+++ src/modules/hide-course-requests.ts
@@ -5,12 +5,11 @@ const domQuery = () => document.querySelector('#course-requests-btn');
 
 export async function hideCourseRequestsMain() {
   const courseRequestsButton = await waitForLoad(domQuery);
-  console.log('Hidden');
   courseRequestsButton.style.display = 'none';
 }
 
 export default registerModule('{683a9158-9030-4015-bca6-3513b21caf72}', {
   name: 'Hide Course Requests Button',
-  description: 'Hide course requests button until course requests are opened.',
+  description: 'Hide course requests button until courses are available.',
   main: hideCourseRequestsMain,
 });
