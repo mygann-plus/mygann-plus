d48b67694727c96e7beaf11f355aa2594ab35462
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-06-14 16:15:17 -0400

    Co-authored-by: 22shirsh@gannacademy.org <22shirsh@gannacademy.org>

--- src/modules/quizlet.tsx
+++ src/modules/quizlet.tsx
@@ -3,31 +3,43 @@
 import registerModule from '~/core/module';
 import { waitForLoad } from '~/utils/dom';
 
-// find url and get quizlet code
+const domQuery = {
+  detail: () => document.querySelector('.bb-tile-content-section') as HTMLElement,
+  parent: () => document.querySelector('#original-layout-container') as HTMLElement,
+};
 
-
-
-const quizCode: String;
-
-const panel = (<div id="quizlet-panel" class="col-md-6 bb-page-content-tile-column">
-  <section class="bb-tile">
-    <div class="bb-tile-title">
-      <div class="_2gWpUJuiJeyc-AxRks32ng">
-        <h2 class="bb-tile-header">STUDY UP (but known by Sam as &quotQuizlets&quot)</h2>
-      </div>
-    </div>
-    <div class="bb-tile-content">
-      <div>
-        <div class="bb-tile-content-section">
-          <iframe src={`https://quizlet.com/${quizCode}/match/embed?i=1n65lh&x=1jj1`} height="500" width="100%" style="border:0"></iframe>
+async function makePanel(quizCode) {
+  let parent = await waitForLoad(domQuery.parent);
+  parent.appendChild(
+    <div id="quizlet-panel" class="col-md-6 bb-page-content-tile-column">
+      <section class="bb-tile">
+        <div class="bb-tile-title">
+          <h2 class="bb-tile-header">Attached Quizlets</h2>
         </div>
-      </div>
-    </div>
-  </section>
-</div>);
+        <div class="bb-tile-content">
+          <div>
+            <div class="bb-tile-content-section">
+              <iframe src={`https://quizlet.com/${quizCode}/match/embed?i=1n65lh&x=1jj1`} height="500" width="100%" style="border:0"></iframe>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>,
+  );
+}
+
+async function quizletMain() {
+  const text = (await waitForLoad(domQuery)).innerText;
+
+  if (text.includes('https://quizlet.com/')) {
+    let split = text.split('/');
+    let quizCode = split[split.indexOf('quizlet.com') + 1];
+  }
+  makePanel(quizCode);
+}
 
 export default registerModule('{440b57cd-80bd-457c-b236-3aba21bad418}', {
-  name: 'In Assignment Quizlets',
-  description: 'Quizlet links will automatically open in side panel.',
+  name: 'Open Quizlets in Side Panel',
+  description: 'Quizlet links will automatically open in a side panel for easy access.',
   main: quizletMain,
 });
