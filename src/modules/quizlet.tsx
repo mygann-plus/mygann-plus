87a9123517f5b99dc8be6d20a723965684664518
quokkasAreTheBest <45802154+quokkasAreTheBest@users.noreply.github.com>
2020-06-15 13:39:30 -0400

    ds

--- src/modules/quizlet.tsx
+++ src/modules/quizlet.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import registerModule from '~/core/module';
-import { waitForLoad } from '~/utils/dom';
+import { waitForLoad, createElement } from '~/utils/dom'; // Tell Ilan that createElement is needed in tsx
+
 
 const domQuery = {
   detail: () => document.querySelector('.bb-tile-content-section') as HTMLElement,
   parent: () => document.querySelector('#original-layout-container') as HTMLElement,
 };
 
-async function makePanel(quizCode) {
+async function makePanel(quizCode : String) {
   const panel = (
     <div id="quizlet-panel" class="col-md-6 bb-page-content-tile-column">
       <section class="bb-tile">
@@ -18,7 +19,7 @@ async function makePanel(quizCode) {
         <div class="bb-tile-content">
           <div>
             <div class="bb-tile-content-section">
-              <iframe src={`https://quizlet.com/${quizCode}/match/embed?i=1n65lh&x=1jj1`} height="500" width="100%" style="border:0"></iframe>
+              <iframe src={`https://quizlet.com/${quizCode}/flashcards/embed?i=1n65lh&x=1jj1`} height="500" width="100%"></iframe>
             </div>
           </div>
         </div>
@@ -28,18 +29,20 @@ async function makePanel(quizCode) {
 
   let parent = await waitForLoad(domQuery.parent);
   parent.appendChild(panel);
+
 }
 
 async function quizletMain() {
   const text = (await waitForLoad(domQuery.detail)).innerText;
-  console.log(text)
   let quizCode = null;
 
   if (text.includes('https://quizlet.com/')) {
     let split = text.split('/');
     quizCode = split[split.indexOf('quizlet.com') + 1];
   }
+
   await makePanel(quizCode);
+
 }
 
 export default registerModule('{440b57cd-80bd-457c-b236-3aba21bad418}', {
