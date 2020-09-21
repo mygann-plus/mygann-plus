fa1e27ba6febfc16d37a21c47f878b06b4adb9bc
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-09-21 16:50:13 -0400

    Changed getCDNImageUrl to work with avatars module

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -3,7 +3,13 @@ import { getUserProfile } from './user';
 /* eslint-disable import/prefer-default-export */
 
 export async function getCDNImageUrl(fileName: string) {
-  const userProfileHref = await getUserProfile();
-  const ftpId = userProfileHref.split('/ftpimages/')[1].split('/')[0];
+  // const userProfileHref = await getUserProfile();
+  // const ftpId = userProfileHref.split('/ftpimages/')[1].split('/')[0];
+
+  // const context = await (await fetch('https://gannacademy.myschoolapp.com/api/webapp/schoolcontext')).json();
+  // .then(r => r.json);
+  const response = await fetch('https://gannacademy.myschoolapp.com/api/webapp/schoolcontext');
+  const context = await response.json();
+  const ftpId = context.SchoolInfo.SchoolId;
   return `https://gannacademy.myschoolapp.com/ftpimages/${ftpId}/${fileName}`;
 }
