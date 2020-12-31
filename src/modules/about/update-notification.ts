26e09e604def330226677c6edaa161f27d88bd62
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2020-12-31 11:58:59 -0500

    Update Notification Fixed

--- src/modules/about/update-notification.ts
+++ src/modules/about/update-notification.ts
@@ -1,12 +1,7 @@
-import storage from '~/utils/storage';
-
-const SHOW_NOTIFICATION_KEY = 'show_update_notification';
-const SCHEMA_VERSION = 1;
-
 export async function shouldShowNotification() {
-  const shouldShow = await storage.get(SHOW_NOTIFICATION_KEY, SCHEMA_VERSION);
-  return shouldShow !== false;
+  return localStorage.getItem('MyGannPlusUpdated') === 'true';
 }
+
 export function disableNotification() {
-  return storage.set(SHOW_NOTIFICATION_KEY, false, SCHEMA_VERSION);
+  localStorage.setItem('MyGannPlusUpdated', 'false');
 }
