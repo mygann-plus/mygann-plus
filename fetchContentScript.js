15d40c5fc6506556fdb7dc4e3b81a2da20df1770
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-01-18 17:11:25 -0500

    I'm pretty sure this is a fail

--- fetchContentScript.js
+++ fetchContentScript.js
@@ -1,11 +1,16 @@
 console.log('Yes that\'s correct I\'m running perfectly and world is amazing in every way and to demonstrate I\'m printing this wonderful piece of writing that brings happinness, love, empathy, kindness, humility, faith, oppertunity, gratitude, understanding, humor, patience, and joy to the readers big warm heart.');
 
+const eventNOTDEPRECATED = new Event('CheckTheDot');
+
 eval(localStorage.getItem('MyGannPlus')); // try running it immediatly
 (async () => {
   const res = await fetch('https://mygann-plus-bookmarklet.surge.sh/dist/content-script.js');
   const text = await res.text();
   if (localStorage.getItem('MyGannPlus') === null) eval(text); // if there was nothing to run run it now
-  if (localStorage.getItem('MyGannPlus') !== text) localStorage.setItem('MyGannPlusUpdated', 'true');
+  if () {
+    localStorage.setItem('MyGannPlusUpdated', 'true');
+    document.dispatchEvent(eventNOTDEPRECATED);
+  }
   localStorage.setItem('MyGannPlus', text); // update the localStorage
 })();
 
