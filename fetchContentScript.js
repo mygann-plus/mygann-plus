b4579d1762dc3c092eb337449e24ec2554b6ff0f
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-02-12 14:25:13 -0500

    Updated fetchContentScript to make it easy to see what the minified one is doing

--- fetchContentScript.js
+++ fetchContentScript.js
@@ -1,17 +1,16 @@
-// console.log('Yes that\'s correct I\'m running perfectly and world is amazing in every way and to demonstrate I\'m printing this wonderful piece of writing that brings happinness, love, empathy, kindness, humility, faith, oppertunity, gratitude, understanding, humor, patience, and joy to the readers big warm heart.');
+/* eslint-disable no-eval, no-console */
+console.log('Yes that\'s correct I\'m running perfectly and world is amazing in every way and to demonstrate I\'m printing this wonderful piece of writing that brings happinness, love, empathy, kindness, humility, faith, oppertunity, gratitude, understanding, humor, patience, and joy to the readers big warm heart.'); // eslint-disable-line max-len
 
-// const eventNOTDEPRECATED = new Event('CheckTheDot');
+async function main() {
+  const cachedJs = localStorage.getItem('MyGannPlus');
+  eval(cachedJs); // try running it immediatly
+  const res = await fetch('https://mygann-plus-bookmarklet.surge.sh/dist/content-script.js'); // where the content script is stored
+  const updatedJs = await res.text(); // turn the fetched code into executable js (still a string)
+  if (!cachedJs) eval(updatedJs); // No code will have been run if cachedJs is empty
 
-// eval(localStorage.getItem('MyGannPlus')); // try running it immediatly
-// (async () => {
-//   const res = await fetch('https://mygann-plus-bookmarklet.surge.sh/dist/content-script.js');
-//   const text = await res.text();
-//   if (localStorage.getItem('MyGannPlus') === null) eval(text); // if there was nothing to run run it now
-//   if () {
-//     localStorage.setItem('MyGannPlusUpdated', 'true');
-//     document.dispatchEvent(eventNOTDEPRECATED);
-//   }
-//   localStorage.setItem('MyGannPlus', text); // update the localStorage
-// })();
+  localStorage.setItem('MyGannPlus', updatedJs); // set it to the updated js for next time
+}
 
-// // minified in fetchContentScript.min.js
+main();
+
+// minified in fetchContentScript.min.js
