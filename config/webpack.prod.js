a869e2eaf4ee77fdacfaf0e11e91574b83421b2c
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-03-16 11:23:09 -0400

    Added Extension Folder for Testing

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,4 +1,10 @@
+const path = require('path');
+
 module.exports = {
   extends: require.resolve('./webpack.common'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist',
+  },
   mode: 'production',
 };
