2e62b8277b4c1f8eea4b4abde33b18c9ed5c5fd6
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-09-01 14:37:09 -0400

    stuff
    Co-authored-by: Sam506944 <Sam506944@users.noreply.github.com>

--- src/modules/title-scroll.ts
+++ src/modules/title-scroll.ts
@@ -2,11 +2,14 @@ import registerModule from '~/core/module';
 
 // move the title text when it's too long
 function scrollTitle() {
-
+  setInterval(() => {
+    let currentTitle = document.title;
+    currentTitle += ` ${currentTitle[0]}`
+  }, 0.05);
 }
 
 async function titleScrollMain() {
-
+  
 }
 
 function unloadTitleScroll() {
@@ -14,10 +17,10 @@ function unloadTitleScroll() {
 }
 
 export default registerModule('{f724b60d-6d47-4497-a71e-a40d7990a2f4}', {
-  name: 'Title Info',
-  description: 'Show more information in the tab title',
+  name: 'Dynamic Title',
+  description: 'Display class time remaining in tab title',
   defaultEnabled: false,
   suboptions: {},
-  init: titleScrollMain,
+  main: titleScrollMain,
   unload: unloadTitleScroll,
 });
