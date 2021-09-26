0cdb66f51c8faefd3b9431475f778e29641f375f
quokkasAreTheBest <45802154+quokkasAreTheBest@users.noreply.github.com>
2021-09-26 19:04:41 -0400

    minutes until class ends calculated correctly

--- src/modules/title-scroll.ts
+++ src/modules/title-scroll.ts
@@ -5,22 +5,29 @@ import { isCurrentTime } from '~/shared/schedule';
 async function titleScrollMain() {
   const date = new Date();
 
+  // the following code should run once per class
+  const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const year = date.getFullYear();
 
   const endpoint = `/api/schedule/MyDayCalendarStudentList/?scheduleDate=${month}%2F${day}%2F${year}`;
   const data = await fetchApi(endpoint);
   const currentClass = data.find((block: any) => isCurrentTime(`${block.MyDayStartTime}-${block.MyDayEndTime}`));
 
   if (!currentClass) return;
-}
 
-function scrollTitle(title: string) {
-  setInterval(() => {
-    title = title.substring(1) + title.charAt(0);
-    document.title = title;
-  }, 100);
+  const endTimeString = `1/1/2001 ${currentClass.MyDayEndTime}`;
+  const endTimeDate: any = new Date(endTimeString);
+
+  // the following code should be running in a loop
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const noon = hours >= 12 ? 'PM' : 'AM';
+
+  const currentDateString = `1/1/2001 ${hours % 12}:${minutes} ${noon}`;
+  const currentTimeDate: any = new Date(currentDateString);
+
+  let title = `MyGann | ${(endTimeDate - currentTimeDate) / 60000}min remaining`;
 }
 
 function unloadTitleScroll() {
