411bb9a6cc744946351a4eb3ef3e389186c2cc8f
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-10-20 18:55:38 -0400

    no more log in podiumApp

--- src/utils/podiumApp.tsx
+++ src/utils/podiumApp.tsx
@@ -1,5 +1,4 @@
 import { createElement } from '~/utils/dom';
-import log from '~/utils/log';
 
 // script body if a function needs further parameters: ({f})(window, {params.map(s => `'${s}'`).join(',')});
 
@@ -10,7 +9,6 @@ export default function runWithPodiumApp(f: (window: any) => void) {
       ({f})(window);
     </script>
   );
-  log('log', script);
   document.head.appendChild(script); // end of head means all the podiumApp scripts run first, but they don't start initializing (in theory if mygann+ loaded slowly they might, so always make sure it works even if mygann+ loads after initializing podiumApp)
   script.remove(); // no need to clutter up the DOM with these script tags
 }
