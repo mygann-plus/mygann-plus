006cade9fdf5da0ebfae21d2c2ffc67a40e87ced
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2021-10-19 21:53:12 -0400

    Updated manifests, dotNumber.

--- src/utils/manifest.ts
+++ src/utils/manifest.ts
@@ -1,7 +1,7 @@
-export const dotNumber = '0';
+export const dotNumber = '1';
 
 const manifest: chrome.runtime.Manifest = {
-  version_name: '1.15.0',
+  version_name: '1.16.0',
   description: 'A collection of modules that improve your MyGann experience',
 } as chrome.runtime.Manifest;
 export default manifest;
