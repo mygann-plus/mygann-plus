aad476f033e8b1d7989799acabed66277fb5df81
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2022-01-04 23:30:29 -0500

    Dynamic Title Work

--- src/modules/dynamic-title.ts
+++ src/modules/dynamic-title.ts
@@ -2,6 +2,7 @@ import registerModule from '~/core/module';
 import { fetchApi } from '~/utils/fetch';
 import { isCurrentTime, to24Hr } from '~/shared/schedule';
 import { timeStringToDate } from '~/utils/date';
+import { UnloaderContext } from '~/core/module-loader';
 
 let cachedSchedule: { schedule: Promise<any[]>, dateString: string };
 function getTodaySchedule() {
@@ -14,6 +15,7 @@ function getTodaySchedule() {
 
     const endpoint = `/api/schedule/MyDayCalendarStudentList/?scheduleDate=${month}%2F${day}%2F${year}`;
     cachedSchedule = { schedule: fetchApi(endpoint), dateString };
+    // cachedSchedule.schedule.then(data => { data[data.length - 1].MyDayEndTime = '11:00 PM'; }); // to test outside school hours
   }
   return cachedSchedule;
 }
@@ -40,43 +42,71 @@ async function getClass() {
   return { endTime, period };
 }
 
-function changeTitle(endTime: number, period: boolean) {
+// check if the title was changed by MyGann+ or not
+function defaultTitle(title?: string) {
+  return !/\d+ min (remaining)|(before next class)/.test(title || document.title);
+}
+
+// to unload and revert the title
+let lastTitle: string;
+function changeTitle(newTitle: string) {
+  if (defaultTitle()) lastTitle = document.title; // if the title is a default one it should be reverted back to it while unloading
+  document.title = newTitle;
+}
+
+function displayTime(endTime: number, period: boolean) {
   const now = Date.now();
-  const minutesRemaining = (endTime - now) / 60e3;
+  const minutesRemaining = Math.ceil((endTime - now) / 60e3);
   if (minutesRemaining >= 0) {
-    document.title = `${minutesRemaining} min ${period ? 'remaining' : 'before next class'}`;
+    changeTitle(`${minutesRemaining} min ${period ? 'remaining' : 'before next class'}`);
   }
   return minutesRemaining;
 }
 
-async function titleScrollMain() {
+const config: MutationObserverInit = { childList: true };
+const observer = new MutationObserver(([mutation], obs) => {
+  const oldTitle = (mutation.removedNodes[0] as Text).data;
+  if (!defaultTitle() || defaultTitle(oldTitle)) return; // if it is now using a custom one or if it just had a defult one don't switch
+
+  obs.disconnect(); // so the observer doesn't fire itself
+  changeTitle(oldTitle); // if the old one was set by MyGann+ bring it back
+  obs.observe(mutation.target, config);
+});
+
+function unloadTitleScroll() {
+  document.title = lastTitle;
+  observer.disconnect();
+}
+
+async function titleScrollMain(opts: void, unloaderContext: UnloaderContext) {
   const { endTime, period } = await getClass();
 
   if (!endTime) {
+    unloadTitleScroll();
     const today = new Date();
     const now = today.getTime();
     const midnight = today.setHours(24, 0, 0, 0);
-    return setTimeout(titleScrollMain, midnight - now); // run again at midnight
+    return setTimeout(titleScrollMain, midnight - now, opts, unloaderContext); // run again at midnight
   }
 
-  changeTitle(endTime, period);
+  displayTime(endTime, period);
+  observer.observe(document.querySelector('title'), config);
 
   let interval = setInterval(() => {
-    if (changeTitle(endTime, period) < 0) {
+    if (displayTime(endTime, period) < 0) {
       clearInterval(interval);
-      titleScrollMain();
+      unloadTitleScroll();
+      titleScrollMain(opts, unloaderContext);
     }
-  }, 30e3);
-}
+  }, 5e3);
 
-function unloadTitleScroll() {
-  titleScrollMain();
+  unloaderContext.addFunction(() => clearInterval(interval));
 }
 
 export default registerModule('{f724b60d-6d47-4497-a71e-a40d7990a2f4}', {
   name: 'Dynamic Title',
   description: 'Displays minutes remaining in class in tab title',
   defaultEnabled: false,
-  main: titleScrollMain,
+  init: titleScrollMain,
   unload: unloadTitleScroll,
 });
