d6d36a6676973b05616d6f3e21fe91e505450558
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2022-03-09 09:40:54 -0500

    removed another recheck

--- src/modules/highlight-current-class/index.ts
+++ src/modules/highlight-current-class/index.ts
@@ -1,8 +1,8 @@
 import registerModule from '~/core/module';
 import { UnloaderContext } from '~/core/module-loader';
 
-import { waitForLoad, insertCss } from '~/utils/dom';
-import { addDayChangeListener, isCurrentClass } from '~/shared/schedule';
+import { insertCss } from '~/utils/dom';
+import { addAsyncDayLoadedListener, isCurrentClass } from '~/shared/schedule';
 
 import style from './style.css';
 import { addMinuteListener } from '~/utils/tick';
@@ -19,51 +19,30 @@ function removeHighlight() {
   }
 }
 
-const domQuery = () => document.getElementById('accordionSchedules')?.children[0]?.children?.length;
-
-async function highlight(blocks: HTMLCollection) {
+async function highlight() {
+  const blocks = document.getElementById('accordionSchedules').children;
   for (const block of blocks) {
     const timeString = (block.children[0].childNodes[0] as Text).data.trim();
     if (await isCurrentClass(timeString)) {
       block.classList.add(selectors.currentClass);
-      // [audit] replace with MutationObserver; extract to shared
       return block;
     }
   }
 }
 
-async function highlightClass() {
-  await waitForLoad(domQuery);
-
-  const getBlocks = () => document.getElementById('accordionSchedules').children;
-  const recheck = (block: Element) => {
-    if (!document.body.contains(block)) {
-      highlight(getBlocks());
-    }
-  };
-
-  const block = await highlight(getBlocks());
-
-  for (let i = 0; i < 20; i++) {
-    setTimeout(() => recheck(block), i * 50);
-  }
-
-}
-
-function highlightCurrentClassMain(opts: void, unloaderContext: UnloaderContext) {
+async function highlightCurrentClassMain(opts: void, unloaderContext: UnloaderContext) {
   const styles = insertCss(style.toString());
   unloaderContext.addRemovable(styles);
 
-  highlightClass();
-
   const interval = addMinuteListener(() => {
     removeHighlight();
-    highlightClass();
+    highlight();
   });
 
   unloaderContext.addRemovable(interval);
 
-  const dayChangeListener = addDayChangeListener(highlightClass);
+  // const dayChangeListener = addDayChangeListener(highlightClass);
+  const dayChangeListener = await addAsyncDayLoadedListener(highlight);
   unloaderContext.addRemovable(dayChangeListener);
 }
 
