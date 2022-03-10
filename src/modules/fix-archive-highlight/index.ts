3f42d5054198c09fd10db9a381e34bdb97a07a03
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2022-03-09 23:07:11 -0500

    fixed fix archive highlight keeping messages highlighted even after leaving (aka gave it an unloadercontext)

--- src/modules/fix-archive-highlight/index.ts
+++ src/modules/fix-archive-highlight/index.ts
@@ -7,17 +7,20 @@
 */
 
 import registerModule from '~/core/module';
+import { UnloaderContext } from '~/core/module-loader';
 
 import { insertCss } from '~/utils/dom';
 
 import style from './style.css';
 
-async function fixArchiveHighlightMain() {
-  insertCss(style.toString());
+async function fixArchiveHighlightMain(opts: void, unloaderContext: UnloaderContext) {
+  const styles = insertCss(style.toString());
+  unloaderContext.addRemovable(styles);
 }
 
 export default registerModule('{5a8a766a-39cf-4c97-b049-02c7cded7372}', {
   name: 'fix.fixArchiveHighlight',
   init: fixArchiveHighlightMain,
   showInOptions: false,
+  affectsGlobalState: true,
 });
