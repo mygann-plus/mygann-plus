a8bf96edee62c6b7a47404fcf0d41ee1e5211ca6
Ilan-Sperber <43863424+Ilan-Sperber@users.noreply.github.com>
2022-12-26 23:17:48 -0500

    update brings back instant update update instantly
    This reverts commit 4618da84c1cb51c8608499430f5133d7a03d25de.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 module.exports = {
   extends: require.resolve('./webpack.common'),
+  output: {
+    path: path.resolve(__dirname, '../test/dist'),
+    publicPath: '/test/dist',
+  },
   mode: 'development',
   devtool: 'source-map',
   watch: true,
