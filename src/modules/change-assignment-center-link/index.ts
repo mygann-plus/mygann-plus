0cb0ec37a7d57ce9b3c45c2d5c8ec67f4b297278
apers00n <natanameyer@gmail.com>
2024-08-11 16:57:02 -0400

    now also changes link in dropdown

--- src/modules/change-assignment-center-link/index.ts
+++ src/modules/change-assignment-center-link/index.ts
@@ -2,9 +2,7 @@ import registerModule from '~/core/module';
 
 import { waitForLoad } from '~/utils/dom';
 
-const domQuery = () => document.getElementById('assignment-center-btn');
-
-async function changeLink() {
+async function changeLink(domQuery: () => Element) {
   document.querySelector('body');
   const linkButton = await waitForLoad(domQuery);
   try {
@@ -12,10 +10,16 @@ async function changeLink() {
       'href',
       '/app/student#studentmyday/assignment-center',
     );
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }
+  } catch (error) {}
+}
+
+async function changeLinks() {
+  const barIcon = () => document.getElementById('assignment-center-btn');
+  const dropDown = () => document.querySelector(
+    '#topnav-containter > ul > li.oneline.parentitem.first > div.subnav.sec-75-bordercolor.white-bgc.sky-nav.nav-visible > ul > li:nth-child(3) > a',
+  );
+  await changeLink(barIcon);
+  await changeLink(dropDown);
 }
 
 export default registerModule('{eec0a74b-0e45-4ed6-bb40-dbc1955b4dc6}', {
@@ -23,6 +27,6 @@ export default registerModule('{eec0a74b-0e45-4ed6-bb40-dbc1955b4dc6}', {
   description:
     // eslint-disable-next-line max-len
     'Changes the url of the "Assignment Center" button to be the original one instead of the new one added in 2024-2025',
-  main: changeLink,
+  main: changeLinks,
   defaultEnabled: false,
 });
