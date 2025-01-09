952c8ee100d830dbc541466b0581d726c8d42418
apers00n <natanameyer@gmail.com>
2025-01-08 20:53:22 -0500

    cleanup the extension. still don't push live yet, but it doesn't have swears in the description now + has an actual uuid of its own lol

--- src/modules/platformer/index.tsx
+++ src/modules/platformer/index.tsx
@@ -3,14 +3,13 @@ import { UnloaderContext } from '~/core/module-loader';
 import createCanvas from './main';
 
 function platformerMain() {
-  console.log('placeholder');
-  createCanvas()
+  createCanvas();
 }
 
-export default registerModule('{50310672-9670-48a4-8261-2868a426ace6}', {
+export default registerModule('{71488e60-41d0-47d4-bd03-5dfc4c6e1576}', {
   name: 'Platformer',
-  description: 'its a platformer bitch',
+  description: 'Turn MyGann into a platformer game! Yellow boxes are jump platforms and green are normal platforms. Disable the game from the mygann+ settings.', // TODO: add a keybind or button to close the game.
   init: platformerMain,
-  // unload: unloadMyDayShortcut,
-  defaultEnabled: true,
+  // unload: , // NOTE: i feel like this one does need an unloader, mainly deleting the canvas and clearing stuff. I think there's a function in main.tsx of the game to stop it all and delete it. need to stop intervals, delete all elements, etc.
+  defaultEnabled: false,
 });
